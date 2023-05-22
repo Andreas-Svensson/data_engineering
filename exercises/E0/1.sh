71af6c958d9ea969a32180eef015e28dbb64bc9d
Andreas Svensson <andreas.svensson@iths.se>
2023-05-22 18:21:24 +0200

    exercise 0 part 1 done

---
+++ exercises/E0/1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# a) make dir
+mkdir cool_notes
+
+# b) move note files
+mv note* cool_notes/
+
+# c) delete note4
+rm cool_notes/note4
+
+# d) change dir and list files
+cd cool_notes
+echo -e "\nFiles in cool_notes folder:"
+ls
+
+# e) move note3 to parent
+mv note3.txt ..
+
+# f) navigate to parent
+cd ..
+
+# g) list all files
+echo -e "\nFiles in parent folder:"
+ls -a
+
+# h) rename note3
+mv note3.txt note_home.txt
+
+
