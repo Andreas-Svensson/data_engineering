77c9770df405c8f5c503fb1049cf47d468b02c6a
Andreas Svensson <andreas.svensson@iths.se>
2023-08-18 12:33:58 +0200

    added test functions

--- exercises/e6_cicd_tools/test_print_joke.py
+++ exercises/e6_cicd_tools/test_print_joke.py
@@ -1,15 +1,24 @@
-from print_joke import get_random_reaction
+from print_joke import get_random_reaction, reactions
 
 
 def test_get_random_reaction_type():
     reaction = get_random_reaction()
-    assert False # Replace False with a check that makes sure the reaction is a string type
+    assert type(reaction) == str # Replace False with a check that makes sure the reaction is a string type
 
 
 def test_get_random_reaction_repeats():
     # Write a test that checks that multiple calls to get_random_reaction()
     # doesn't give you the same reaction every time
-    pass 
+    for _ in range(1000):
+        reaction1, reaction2 = get_random_reaction(), get_random_reaction()
+        if reaction1 != reaction2:
+            break
+    assert not reaction1 == reaction2
 
 
 # Come up with a test of your own and implement it here.
+def test_reactions_dtype():
+    for reaction in reactions:
+        if type(reaction) != str:
+            break
+    assert type(reaction) == str
